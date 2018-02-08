3b2e3954d7e93ae13efe810454d8c5d145537f57
kfir marouani <kfir.marouani@my6sense.com>
2018-02-08 16:09:00 +0200

    linkedlee - linked list implementation first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# linkedlee
+
+JavaScript implementation of a Linked List with extra power
+
+# Installation 
+
+Simply reference qulee.js or qulee.min.js on your html page
+
+```
+<script src="linkedlee.min.js"></script>
+```
+
+Using it on node.js:
+
+```
+npm install linkedlee --save
+```
+
+# Usage
+
+Linkedlee provides an implementation of a linked list which can be used both in NodeJS applications and on your web application.   
+
+# API
+
+**linkedList.push(item)** - add an item.  
+**linkedList.pushTop(item)** - add an item to the top of the linked list (as the first item)    
+**linkedList.get(index)** - returns the node in the given index.  
+**linkedList.remove(index)** - removes the node in the given index.    
+**linkedList.size()** - return the linked list size.   
+**linkedList.down(index)** - moved the node in the given index down the linked list chain by one.    
+**linkedList.up(index)** - moved the node in the given index up the linked list chain by one.                              
+
+# Node API
+
+**node.next()** - get next node
+**node.prev()** - get previous node
+**node.value()** - get node's value/data
+**node.remove()** - removed the node from the list
+ 
+# Examples
+
+Simple example:
+```
+var LinkedList = require('linkedlee');
+
+linkedList = new LinkedList(*maxSize*); // maxSize is optinal, if not given there is no limit 
+
+linkedList.push('a');
+linkedList.push('b');
+linkedList.push('c');
+
+var node = linkedList.get(0); 
+do {
+    console.log(firstNode.value());
+    node = node.next();
+} while (firstNode)
+// will output: a c b
+
+```
---
+++ src/linkedlee.js
@@ -0,0 +1,145 @@
+(function (global) {
+
+    "use strict";
+
+    function Node(_value, _next, _prev) {
+
+        var value = _value;
+        var next = _next;
+        var prev = _prev;
+
+        function setNext(node) {
+            next = node;
+        }
+
+        function setPrev(node) {
+            prev = node;
+        }
+
+
+        this.next = function () {
+            return next;
+        };
+
+        this.prev = function () {
+            return prev;
+        };
+
+        this.value = function () {
+            return value;
+        };
+
+        this.remove = function () {
+
+            if (next && prev) {
+                next.setPrev(this.prev());
+                prev.setNext(this.next())
+            } else if (next) {
+                next.setPrev(null);
+            } else if (prev) {
+                prev.setNext(null);
+            }
+        };
+
+    }
+
+    Node.prototype.swap = function (node) {
+
+        var tempAPrev = this.prev();
+
+        this.setNext(node.next());
+        this.setPrev(node);
+
+        node.setNext(this);
+        node.setPrev(tempAPrev);
+    };
+
+    function LinkedList(maxSize) {
+
+
+        var nodes = [];
+
+        function swap(indexA, indexB) {
+            var tempValue = nodes[indexB];
+            nodes[indexB] = nodes[indexA];
+            nodes[indexA] = tempValue;
+        }
+
+        function isMaxLengthExceeded() {
+            return !( (maxSize !== null || maxSize === undefined) && maxSize > nodes.length )
+        }
+
+        function getNode(value) {
+            var node = null;
+
+            if (nodes.length === 0) {
+                node = new Node(value, null, null);
+            } else {
+
+                var prevNode = nodes[nodes.length - 1];
+
+                node = new Node(value, null, prevNode);
+                prevNode.setNext(node);
+            }
+
+            return node;
+        }
+
+        this.push = function (value) {
+
+            if (!isMaxLengthExceeded()) {
+                var node = getNode(value);
+                nodes.push(node);
+                return node;
+            } else {
+                return null;
+            }
+        };
+
+        this.pushTop = function (value) {
+            if (!isMaxLengthExceeded()) {
+                var node = getNode(value);
+                nodes.splice(0, 0, node);
+                return node;
+            } else {
+                return null;
+            }
+        };
+
+        this.get = function (index) {
+            return nodes[index];
+        };
+
+        this.remove = function (index) {
+            nodes[index].remove();
+            nodes.splice(index, 1);
+        };
+
+        this.size = function () {
+            return nodes.length;
+        };
+
+        this.down = function (index) {
+            if (nodes[index] && nodes[index + 1]) {
+                nodes[index].swap(nodes[index + 1]);
+            }
+
+            swap(index, index + 1);
+        };
+
+        this.up = function (index) {
+            if (nodes[index] && nodes[index - 1]) {
+                nodes[index].swap(nodes[index - 1]);
+            }
+
+            swap(index, index - 1);
+        };
+    }
+
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = LinkedList;
+    } else {
+        global.LinkedList = LinkedList;
+    }
+
+})(typeof window !== "undefined" ? window : global);
